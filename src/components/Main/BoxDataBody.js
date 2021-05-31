e12503f77de0c9260fb401c0d234e5cdc6817c73
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2021-05-31 22:15:45 +0700

    update style

--- src/components/Main/BoxDataBody.js
+++ src/components/Main/BoxDataBody.js
@@ -7,8 +7,6 @@ export class BoxDataBody extends Component {
   };
   render() {
     const { typeData, dataCovid } = this.props;
-    console.log("🚀 ~ file: BoxDataBody.js ~ line 10 ~ BoxDataBody ~ render ~ dataCovid", dataCovid)
-    
     return (
       <div className="box-data__body">
         {this.checkTypeData(typeData)}
