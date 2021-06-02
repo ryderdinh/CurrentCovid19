cb76116a4a7db03a8f8f57a4e89c8004a222321a
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2021-06-02 18:09:35 +0700

    update pwa

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers/state";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
@@ -14,4 +16,12 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
